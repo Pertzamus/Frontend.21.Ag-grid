c2aee97641208e2083044fc2dee2062606ba4037
Pertzamus <korhonen.pertti@hotmail.com>
2022-03-26 20:35:46 +0200

    tehtävää tehty eteenpäin

---
+++ ag-grid/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function App() {
+  const [todo, setTodo] = useState({description: '', date: '', priority:''});
+  const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = (event) => {
+    setTodos([...todos, todo]);
+  }
+
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+        setTodos(todos.filter((todo, index) =>
+            index !== gridRef.current.getSelectedNodes()[0].childIndex))
+            }
+            else {
+              alert('Select row first');
+             }
+            }
+
+  const columns = [
+    {headerName: 'Date', field: 'date', sortable: true},
+    {headerName: 'Description', field: 'description', sortable: true},
+    {headerName: 'Priority', field: 'priority', sortable: true, filter: true,
+    cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}}
+  ]
+
+  return (
+    <div>
+      <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description}/>
+      <input type="text" onChange={inputChanged} placeholder="Date" name="date" value={todo.date}/>
+      <input type="text" onChange={inputChanged} placeholder="Priority" name="priority" value={todo.priority}/>
+      <button onClick={addTodo}>Add</button>
+      <button onClick={deleteTodo}>Delete</button>
+      <div className="ag-theme-material"
+       style={{
+         height: '700px',
+          width: '80%',
+          margin: 'auto'}}
+          >
+    <AgGridReact
+    ref={gridRef}
+    onGridReady={ params => gridRef.current = params.api }
+    rowSelection="single"
+    columnDefs={columns}
+    rowData={todos}
+    >
+    </AgGridReact>
+    </div>
+    </div>
+  );
+};
+
+export default App;
